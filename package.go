a7592e6f870282b5288a34dddf15a312785990c6
ss-jam <55657468+ss-jam@users.noreply.github.com>
2019-10-29 07:38:37 -0400

    Initial Commit

---
+++ package.go
@@ -0,0 +1,12 @@
+package main
+
+// From the go tour package.go file for the discussion in file and program structure
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
